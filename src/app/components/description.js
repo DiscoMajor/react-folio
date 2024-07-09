7b391fbc00b5e5e540e52abdb25750a63ab03bae
Victor <coolstorybrotherz@gmail.com>
2024-07-09 13:02:51 +0200

    Some changes

--- src/app/components/description.js
+++ src/app/components/description.js
@@ -1,20 +1,26 @@
-import { FaReact } from "react-icons/fa";
+"use client";
+import { FaReact } from "react-icons/fa6";
+import { useTypewriter } from "react-simple-typewriter";
 
 function AboutMe() {
+    const [typingEffect] = useTypewriter({
+        words: [` Victor.`, ` a React Developer.`],
+        loop: {},
+        typeSpeed: 100,
+        deleteSpeed: 80,
+    });
     return (
-        <section className="min-h-screen">
-            <div className="text-center p-10 py-10">
-                <h2 className="matricule text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">Victor Lobbé</h2>
-                <h3 className="text-2xl py-2 dark:text-white md:text-3xl">Web Developper</h3>
-                <p className="text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-xl">
-                    Welcome to my React <FaReact className="h-20 w-25" /> - TailWind Folio 🍃
+        <section class="min-h-screen flex relative">
+            <div class="text-center p-10 py-10 flex-auto ">
+                <h2 class="text-5xl py-2 font-medium md:text-5xl text-teal-600  dark:text-teal-400 ">
+                    I'm
+                    <span>{typingEffect}</span>
+                </h2>
+                <p class="text-md py-3 mt-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-xl">
+                    Welcome to my <FaReact class="inline-block text-teal-500 md:text-3xl" /> TailWind Folio 🍃
                 </p>
-                <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400"></div>
             </div>
         </section>
     );
 }
 export default AboutMe;
-
-//! REVOIR TAILWIND + REACTICONS comment le mettre à la suite et le positionner.
-//! Idem, voir pour ajouter un nom de class pour modifier le CSS (je veux faire un after sous mon texte H2)
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,14 +1,39 @@
 function Navibar() {
     return (
-        <nav className="py-10 m-10 flex justify-between">
-            <h1 className="text-xl">Welcome, take a sit.</h1>
-            <ul className="flex items-center">
+        <nav class="py-5 m-5 flex justify-between">
+            <h1 class="text-xl font-bold">My Portfolio</h1>
+            <ul class="flex items-center">
                 <li>
                     <a
-                        className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                        class="bg-gradient-to-r from-emerald-500 text- to-green-600 text-white px-4 py-2 border-none rounded-md ml-8"
+                        href="#"
+                    >
+                        About Me
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        class="bg-gradient-to-r from-emerald-500 text- to-green-600 text-white px-4 py-2 border-none rounded-md ml-8"
+                        href="#"
+                    >
+                        Experiences
+                    </a>
+                </li>
+                <li>
+                    <a
+                        class="bg-gradient-to-r from-emerald-500 text- to-green-600 text-white px-4 py-2 border-none rounded-md ml-8"
                         href="#"
                     >
                         Me contacter !
+                    </a>
+                </li>
+                <li>
+                    <a
+                        class="bg-gradient-to-r from-emerald-500 text- to-green-600 text-white px-4 py-2 border-none rounded-md ml-8"
+                        href="#"
+                    >
+                        Contact
                     </a>
                 </li>
             </ul>
